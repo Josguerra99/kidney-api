6d3402913bd561ebc9f7e39da901885e9c1c6e8e
Jose Guerra <josguerra99@gmail.com>
2021-08-22 16:27:22 -0600

    Test

--- PHP-db.sql
+++ PHP-db.sql
@@ -1,35 +1,19 @@
 
-CREATE TABLE kidney_donor(
-	id VARCHAR(11) PRIMARY KEY,
-	patient_id VARCHAR(100) NOT NULL,
-	name VARCHAR(100),
-	address VARCHAR(100),
-	phone VARCHAR(100),
-	birthdate VARCHAR(100),
-	sex VARCHAR(100),
-	bloodgroup VARCHAR(100),
-	FOREIGN KEY (patient_id) REFERENCES patient(patient_id)
 
-);
+SET SQL_SAFE_UPDATES = 0;
+USE kidney_hope;
 
-CREATE TABLE test_info(
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	patient_id VARCHAR(100),
-	donor_id VARCHAR(100),
-	ant1 VARCHAR(10),
-	ant2 VARCHAR(10),
-	ant3 VARCHAR(10),
-	ant4 VARCHAR(10),
-	ant5 VARCHAR(10),
-	ant6 VARCHAR(10),
-	bloodgroup VARCHAR(10),
-	weight FLOAT,
-	birthdate VARCHAR(100),
-	sex VARCHAR(100),
-	FOREIGN KEY (patient_id) REFERENCES patient(patient_id),
-	FOREIGN KEY (donor_id) REFERENCES kidney_donor(id)
-);
+DELETE FROM paciente;
+DELETE FROM donador;
+DELETE FROM antigeno;
 
+USE hospital;
+DELETE FROM kidney_donor;
+DELETE FROM test_info;
+DELETE FROM patient_parents;
+DELETE FROM patient;
+
+SET SQL_SAFE_UPDATES = 1;
 
 /** ACTIVAR EL MODULO EN LA BASE DE DATOS **/
 UPDATE hospital SET 
